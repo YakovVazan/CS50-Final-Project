e8511ebcfc6a743406043bafa24d89b1e5f6592a
YakovVazan <68118450+YakovVazan@users.noreply.github.com>
2023-08-18 17:33:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ init_db()
 
 @app.route("/")
 def index():
-    return render_template("main_layout.html")
+    return render_template("index.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
