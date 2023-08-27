c0df8ff54244d55e2c0ff2f37f421c03539a2b0a
YakovVazan <68118450+YakovVazan@users.noreply.github.com>
2023-08-27 14:16:10 +0300

    Leaving Socials for now

--- socials/x/config/secrets.py
+++ socials/x/config/secrets.py
@@ -1,14 +1,17 @@
 x_configs = {
-    "API_Key": "3bv7h63PCmU5EhOejNoCluchm",
-    "API_Key_Secret": "XWNVC0AWPJLHAcO8pj3l9OEj1AFgaLLDXaI38nNse6krwra1f4",
+    # Think of the API key as the user name that represents your App when making API requests. It helps us verify who you are.
+    "API_Key": "bQSShkVOGyOSZPEYPYDsBja2k",
+    # Your API Key Secret is like a password and helps verify your API Key. This will be one of the last times you'll see it displayed, so remember to save it in a safe place.
+    "API_Key_Secret": "aTYRZEFoNe3Y5jH9IfRExQ8yHXPw26ZWHiMqmhtHFUlEKcMVeu",
+    # An Access Token used in authentication that allows you to pull specific data.
+    "Bearer_Token": "AAAAAAAAAAAAAAAAAAAAAB2ApgEAAAAA71ZEB4ft870a2A0DE3mMgkytgmE%3DCM1ELlX4KIIgr6BeLsWFVnPL6JtMX2L8oQCEMkm1q819zz5Fm9",
+    # Think of your Client ID as the user name that allows you to use OAuth 2.0 as an authentication method.
+    "Client_ID": "RjRINEVTdlQ4S0d6NmUyV2VuMmY6MTpjaQ",
+    # Think of your Client Secret as the password that allows you to use OAuth 2.0 as an authentication method.
+    "Client_Secret": "NjBW23oTdg0Js7dtgnvv4_aT3N826xEJRq4WV5t2AyHjrz_NyO"
+}
 
-    "Client_ID": "dzZxcTRFNmI2X2NuOUJmYkRTems6MTpjaQ",
-    "Client_Secret": "kL75DpGfQNvS6XfFpjxhaR9SLbsiHx9wkndU14KRyUwLJnqbOr",
-
-    "Access_Token": "1692067006642606080-5iExExwAOesxUtZSkq6gQ6AKp3Y63c",
-    "Access_Token_Secret": "bj4ECOTTvs0KGmTYXhkhBSfJ4KbXDBghJmBDpgw08hQEU",
-
-    "Bearer_Token": "AAAAAAAAAAAAAAAAAAAAALnFpQEAAAAA4r3dLGMV34YsfxnmltRmh3Rbcdw%3DouH1aBebkRwPZPLi9qjg3RKpUl65Y5RBi8bI0Zqst3KEPE2HuL",
-
-    "Tweeter_code": "becrI2-rogmic-bepbyd"
+telegram_config = {
+    #  Token to access the HTTP API
+    "Token": "6387343899:AAGebZ1kbRd99Gxo5K0rxp_WpHU_yoYmq3w"
 }
