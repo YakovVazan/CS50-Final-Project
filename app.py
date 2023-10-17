b1ec4a7ccb8dad5cfd2e67c9e152572a30cdc66f
YakovVazan <68118450+YakovVazan@users.noreply.github.com>
2023-10-17 14:10:02 +0300

    Layouts for twitter

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from data import socials
 from datetime import datetime
 import requests
 import json
+from requests_oauthlib import OAuth1Session
 from socials.Telegram.secrets import token
+from socials.X.secrets import keys_and_tokens
 
 # Configure flask app
 app = Flask(__name__)
@@ -482,7 +484,8 @@ def available_apps():
             app_name = request.form.get("app-name")
             if app_name == "Telegram":
                 return redirect(url_for("telegram_login"))
-            # Add here more social media's login's logics
+            elif app_name == "Twitter":
+                return redirect(url_for("twitter_login"))
             else:
                 return render_template("error.html", error_message=f"{app_name} is not available on SocialHub yet.", error_code=503)
     else:
@@ -528,6 +531,95 @@ def telegram_login():
             conn.commit()
             conn.close()
             return redirect("/")
+    else:
+        return redirect("/")
+
+
+@app.route("/available_apps/twitter")
+def twitter_login():
+    if session:
+        print(keys_and_tokens)
+        consumer_key = keys_and_tokens["API Key"]
+        consumer_secret = keys_and_tokens["API Key Secret"]
+
+        # Get database
+        conn = get_db_connection()
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+
+        twitter_details = cursor.execute(
+                    "SELECT * FROM socials WHERE user_id = (?) AND name = (?)", (session["user_id"], "Twitter")).fetchall()
+
+        conn.close()
+
+        if (twitter_details):
+            access_token = twitter_details[0]["access_token"]
+            access_token_secret = twitter_details[0]["access_token_secret"]
+        else:
+            # Get request token
+            request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+            oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+            try:
+                fetch_response = oauth.fetch_request_token(request_token_url)
+            except ValueError:
+                print(
+                    "There may have been an issue with the consumer_key or consumer_secret you entered."
+                )
+
+            resource_owner_key = fetch_response.get("oauth_token")
+            resource_owner_secret = fetch_response.get("oauth_token_secret")
+            print("Got OAuth token: %s" % resource_owner_key)
+
+            # Get authorization
+            base_authorization_url = "https://api.twitter.com/oauth/authorize"
+            authorization_url = oauth.authorization_url(base_authorization_url)
+            print("Please go here and authorize: %s" % authorization_url)
+            verifier = input("Paste the PIN here: ")
+
+            # Get the access token
+            access_token_url = "https://api.twitter.com/oauth/access_token"
+            oauth = OAuth1Session(
+                consumer_key,
+                client_secret=consumer_secret,
+                resource_owner_key=resource_owner_key,
+                resource_owner_secret=resource_owner_secret,
+                verifier=verifier,
+            )
+            oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+            access_token = oauth_tokens["oauth_token"]
+            access_token_secret = oauth_tokens["oauth_token_secret"]
+
+        # Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
+        payload = {
+            "text": "1"}
+
+        # Make the request
+        oauth = OAuth1Session(
+            consumer_key,
+            client_secret=consumer_secret,
+            resource_owner_key=access_token,
+            resource_owner_secret=access_token_secret,
+        )
+
+        # Making the request
+        response = oauth.post(
+            "https://api.twitter.com/2/tweets",
+            json=payload,
+        )
+
+        if response.status_code != 201:
+            raise Exception(
+                "Request returned an error: {} {}".format(
+                    response.status_code, response.text)
+            )
+
+        print("Response code: {}".format(response.status_code))
+
+        # Saving the response as JSON
+        json_response = response.json()
+        print(json.dumps(json_response, indent=4, sort_keys=True))
     else:
         return redirect("/")
 
---
+++ socials/X/secrets.py
@@ -0,0 +1,25 @@
+keys_and_tokens = {
+    # Think of these as the user name and password that represents your App when making API requests.
+
+    # Think of the API key as the user name that represents your App when making API requests.
+    # It helps us verify who you are.
+    "API Key": "DNZXpk2vPYyrH2Pj8LhveLKT8",
+
+    # Your API Key Secret is like a password and helps verify your API Key.
+    # This will be one of the last times you'll see it displayed, so remember to save it in a safe place.
+    "API Key Secret": "pRVUbw4qnszTtAVhIdBtlcRFVwZyTlJ9PwqruUWG2qnDk7RdgP",
+
+    "Access Token": "1692067006642606080-VwivAGM152bmcbuwwdUsdLtXc815NG",
+
+    "Access Token Secret": "p6oNMetv4emOxfLEE0xx9nBeNLeypwNgZZ6cXpfQ3ZZHy",
+
+    # An Access Token used in authentication that allows you to pull specific data.
+    # Bearer Token authenticates requests on behalf of your developer App.
+    "Bearer Token": "AAAAAAAAAAAAAAAAAAAAAEHDqQEAAAAAXYCJvu%2Faye92vXt259%2BSFMIaPkE%3DSfCzU80mSSqPkL8Sf0pxH0mSfi4slATgI5H4mQBEdUBYUFZM3K",
+
+    # Think of your Client ID as the user name that allows you to use OAuth 2.0 as an authentication method.
+    "Client ID": "eXhfU2Nib281Vmo2aktFdTZHbl86MTpjaQ",
+
+    # Think of your Client Secret as the password that allows you to use OAuth 2.0 as an authentication method.
+    "Client Secret": "8oBHF3tVPJYIsJipeHs0eR6cDC8yogSYgNzTqIhv7cgcwOB5XF"
+}
