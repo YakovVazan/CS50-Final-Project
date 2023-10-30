2dbdde81fdcabf7ea54df2d3d3fecbd843212cc3
YakovVazan <yakovvazan@gmail.com>
2023-10-30 14:25:44 +0200

    more responsiveness, drop direction, home icon

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,18 @@ import "./edit_scheduled_posts.js";
 import "./posts_backward.js";
 import "./dynamic_chat.js";
 import "./social_media_script.js";
+
+// Set direction of app drop menu
+function applyClassBasedOnScreenSize() {
+  var element = document.getElementById("set-drop-direction");
+
+  if (window.innerWidth <= 768) {
+    element.classList.add("dropup");
+    element.classList.remove("dropdown");
+  } else {
+    element.classList.remove("dropup");
+    element.classList.add("dropdown");
+  }
+}
+applyClassBasedOnScreenSize();
+window.addEventListener("resize", applyClassBasedOnScreenSize);
