a49368f0bacfdb2931c485d567585d34e5f6aaf3
YakovVazan <yakovvazan@gmail.com>
2023-12-05 15:28:18 +0200

    modular blueprints added

---
+++ blueprints/admin/dashboard_and_data.py
@@ -0,0 +1,35 @@
+import sqlite3
+from blueprints.db.db import get_db_connection
+from flask import Blueprint, session, render_template, redirect, jsonify
+
+dachboard_and_data_bp = Blueprint(
+    "dachboard_and_data_bp", __name__, template_folder="../../templates")
+
+@dachboard_and_data_bp.route("/dashboard")
+def dashboard():
+    if session and session["app_owner"]:
+        return render_template("dashboard.html")
+    else:
+        return redirect("/login")
+
+
+@dachboard_and_data_bp.route('/app_database')
+def app_database():
+    data = {}
+
+    conn = get_db_connection()
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    data["users"] = [dict(user)
+                     for user in cursor.execute("SELECT * FROM users").fetchall()]
+    data["posts"] = [dict(post) for post in cursor.execute(
+        "SELECT * FROM messages").fetchall()]
+    data["scheduled_posts"] = [dict(scheduled_post) for scheduled_post in cursor.execute(
+        "SELECT * FROM scheduled_posts").fetchall()]
+    data["linked_accounts"] = [dict(linked_account) for linked_account in cursor.execute(
+        "SELECT * FROM socials").fetchall()]
+
+    conn.close()
+
+    return jsonify(data)
---
+++ blueprints/db/db.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+db_path = "social_hub.db"
+
+
+def get_db_connection():
+    return sqlite3.connect(db_path)
+
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # Create users table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            email_address TEXT,
+            authenticated NUMERIC NOT NULL,
+            hash TEXT NOT NULL
+        )
+    """)
+    # Create messages table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY,
+            content TEXT NOT NULL,
+            date DATE NOT NULL,
+            is_scheduled BOOLEAN NOT NULL,
+            schedule_date DATE,
+            social_platforms TEXT NOT NULL,
+            user_id INTEGER REFERENCES users(id)
+        )
+    """)
+    # Create scheduled_posts table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS scheduled_posts (
+            id INTEGER PRIMARY KEY,
+            content TEXT NOT NULL,
+            scheduling_date DATE NOT NULL,
+            execution_date DATE NOT NULL,
+            social_platforms TEXT NOT NULL,
+            user_id INTEGER REFERENCES users(id)
+        )
+    """)
+    # Create notifications table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS notifications (
+            id INTEGER PRIMARY KEY,
+            content TEXT NOT NULL,
+            codeColor TEXT NOT NULL,
+            date DATE NOT NULL,
+            user_id INTEGER REFERENCES users(id)
+        )
+    """)
+    # Create socials table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS socials (
+            index_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            social_id TEXT NOT NULL,
+            user_id INTEGER REFERENCES users(id)
+        )
+    """)
+    conn.commit()
+    conn.close()
