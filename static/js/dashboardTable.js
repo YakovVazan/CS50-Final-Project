99caf84b78df6fc08ea66cfcf6d4234ee2a86da6
YakovVazan <yakovvazan@gmail.com>
2023-12-31 14:20:36 +0200

    added filter to dashboard table

--- static/js/dashboardTable.js
+++ static/js/dashboardTable.js
@@ -6,6 +6,8 @@ export function displayUpToDateTable(args) {
 }
 
 async function getDashboardData(args) {
+  unfilter(document.querySelectorAll("tr"));
+
   let appOwnerArea = document.querySelector("#app-owner-area");
 
   if (appOwnerArea) {
@@ -32,7 +34,7 @@ async function getDashboardData(args) {
             ? Array.from(args)
                 .map((arg) => {
                   if (arg["id"] === user["id"]) {
-                    return `<li class="bullet" title="Online">
+                    return `<li id="bullet" class="online-blt" title="Online">
                                 <span class="bullet-span" style="background-color: #ffe207;"></span>
                             </li>`;
                   } else {
@@ -44,14 +46,14 @@ async function getDashboardData(args) {
         }
         ${
           user["authenticated"] === 1
-            ? `<li id="bullet" title="Authenticated">
+            ? `<li id="bullet" class="auth-blt" title="Authenticated">
                 <span class="bullet-span" style="background-color: #67af8e;"></span>
                </li>`
             : ``
         }
         ${
           user["premium"] === 1
-            ? `<li id="bullet" title="Premium">
+            ? `<li id="bullet" class="premium-blt" title="Premium">
                 <span class="bullet-span" style="background-color: #dc3545;"></span>
                </li>`
             : ``
@@ -271,4 +273,58 @@ function setUsersSearchBox() {
       });
     });
   }
+}
+
+let filterOptions = document.querySelectorAll(".filter-option");
+
+if (filterOptions) {
+  filterOptions.forEach((opt) => {
+    opt.addEventListener("click", () => {
+      filterTable(opt.textContent.trim());
+    });
+  });
+}
+
+function filterTable(args) {
+  let allUsersTrs = document.querySelectorAll("tr");
+
+  unfilter(allUsersTrs);
+
+  switch (args) {
+    case "Online":
+      toggleRowVisibility(allUsersTrs, ".online-blt");
+      break;
+    case "Authenticated":
+      toggleRowVisibility(allUsersTrs, ".auth-blt");
+      break;
+    case "Premium":
+      toggleRowVisibility(allUsersTrs, ".premium-blt");
+      break;
+  }
+}
+
+function unfilter(allUsersTrs) {
+  allUsersTrs.forEach((userTr) => {
+    userTr.style.display = "";
+  });
+
+  document.querySelector("#main-filter-button").innerHTML = `
+  Filter
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+    class="bi bi-funnel" viewBox="0 0 16 16">
+      <path
+        d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z" />
+  </svg>
+  `;
+}
+
+function toggleRowVisibility(users, className) {
+  users.forEach((user, index) => {
+    if (user.querySelector(className) === null && index != 0) {
+      user.style.display = "none";
+    }
+  });
+  document.querySelector("#main-filter-button").innerHTML = className
+    .substring(1, className.indexOf("-", 1))
+    .replace(/^[a-z]/, (match) => match.toUpperCase());
 }
