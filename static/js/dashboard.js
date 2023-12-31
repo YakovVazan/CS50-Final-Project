455b154650471d5ee873f9cc0d04b13b8ef9cd81
YakovVazan <yakovvazan@gmail.com>
2023-12-31 08:57:03 +0200

    added button to close dashboard announcement

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,2 +1,22 @@
 import "./dashboardCharts.js";
 import "./dashboardTable.js";
+
+if (localStorage.getItem("SocialHubDashboardAccouncement") === "false") {
+  document.querySelector("#dashboard-announcement-container").style.display =
+    "none";
+} else {
+  document.querySelector("#dashboard-announcement-container").style.display =
+    "flex";
+}
+
+let closeDashboardAnnouncementButton = document.querySelector(
+  "#close-dashboard-announcement-button"
+);
+
+if (closeDashboardAnnouncementButton) {
+  closeDashboardAnnouncementButton.addEventListener("click", () => {
+    document.querySelector("#dashboard-announcement-container").style.display =
+      "none";
+    localStorage.setItem("SocialHubDashboardAccouncement", false);
+  });
+}
