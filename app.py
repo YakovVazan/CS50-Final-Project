0d62aec5f890b113edcde4583556737b3ef40398
YakovVazan <yakovvazan@gmail.com>
2024-04-14 12:31:15 +0300

    fix: wrekzeug error on render.com

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def catch_all(path):
 if __name__ == '__main__':
     init_db()
 
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000,
+                 debug=True, allow_unsafe_werkzeug=True)
